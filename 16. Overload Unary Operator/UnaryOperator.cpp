2773c417b5be6bcda81edd615b01db1343cefae4
RPChinhara-202117 <rpchinhara@gmail.com>
2022-04-12 18:29:59 +0530

    added lab 16, 17

---
+++ 16. Overload Unary Operator/UnaryOperator.cpp
@@ -0,0 +1,50 @@
+// Program to overload unary operator
+
+#include <iostream>
+using namespace std;
+
+class Distance
+{
+private:
+    int meters, centimeters;
+
+public:
+    Distance()
+    {
+        meters = 0;
+        centimeters = 0;
+    }
+
+    Distance(int m, int cm)
+    {
+        meters = m;
+        centimeters = cm;
+    }
+
+    // Displays Distance
+    void display()
+    {
+        cout<<meters<<" m "<<centimeters<<" cm"<<endl;
+    }
+
+    Distance operator- ()
+    {
+        meters = -meters;
+        centimeters = -centimeters;
+        
+        return Distance(meters, centimeters);
+    }
+};
+
+int main()
+{
+    Distance d1(11, 10), d2(5, 1);
+
+    -d1;
+    d1.display();
+
+    -d2;
+    d2.display();
+
+    return 0;
+}
