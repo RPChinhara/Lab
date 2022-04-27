3dcf078b2121205ecb9ff16374098139a4657479
RPChinhara-202117 <rpchinhara@gmail.com>
2022-04-27 11:22:48 +0530

    added lab-11

---
+++ 11. Destructor/Destructor.cpp
@@ -0,0 +1,31 @@
+// Program to demonstrate the concept of destructor
+
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+    int x, y;
+
+public:
+
+    // Constructor
+    Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+    ~Point()
+    {
+        cout<<"x: "<<x<<endl;
+        cout<<"y: "<<y<<endl;
+    }
+};
+
+int main()
+{
+    Point p(1, 3);
+    return 0;
+}
